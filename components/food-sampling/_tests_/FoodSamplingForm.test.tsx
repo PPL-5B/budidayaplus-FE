308b2d1db1f5ff7e5345568abb453f64829b9f28
khalishana <khalisha.hana10@gmail.com>
2025-03-01 23:44:20 +0700

    [RED] Show pop up when quantity above threshold

--- components/food-sampling/_tests_/FoodSamplingForm.test.tsx
+++ components/food-sampling/_tests_/FoodSamplingForm.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import FoodSamplingForm from '../FoodSamplingForm';
 import '@testing-library/jest-dom';
 import React from 'react';
@@ -7,5 +7,17 @@ describe('FoodSamplingForm Component', () => {
   test('Komponen dapat dirender tanpa error', () => {
     render(<FoodSamplingForm pondId="1" cycleId="1" setIsModalOpen={() => {}} />);
     expect(screen.getByLabelText(/Kuantitas Makanan/i)).toBeInTheDocument();
+  });
+
+  test('Menampilkan pop up jika kuantitas makanan lebih dari 1000', () => {
+    render(<FoodSamplingForm pondId="1" cycleId="1" setIsModalOpen={() => {}} />);
+
+    const input = screen.getByLabelText(/Kuantitas Makanan/i);
+    fireEvent.change(input, { target: { value: '1200' } });
+
+    const submitButton = screen.getByText(/Simpan/i);
+    fireEvent.click(submitButton);
+
+    expect(screen.getByText(/Indikator Tidak Sehat!/i)).toBeInTheDocument();
   });
 });
