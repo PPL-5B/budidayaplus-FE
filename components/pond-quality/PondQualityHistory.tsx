643675cdf12c841fd71084020a3b51f7ff72e3c8
codaaa19 <tsabitiphone@gmail.com>
2025-03-09 19:55:59 +0700

    [REFACTOR]: Changing the code structure for revamp

--- components/pond-quality/PondQualityHistory.tsx
+++ components/pond-quality/PondQualityHistory.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import { getPondQualityHistory } from '@/lib/pond-quality';
+'use client'
+
+import React, { useEffect, useState } from 'react';
+import { getPondQualityHistory } from '@/lib/pond-quality/getPondQualityHistory';
 import { DataTable } from '@/components/ui/data-table';
-import { columns } from '@/components/pond-quality';
 import { Waves } from 'lucide-react';
+import PondQualityDashboard from './PondQualityDashboard';
+import { columns } from '@/components/pond-quality';
 
 interface PondQualityHistoryProps {
   pondId: string;
 }
 
-const PondQualityHistory: React.FC<PondQualityHistoryProps> = async ({ pondId }) => {
-  const result = await getPondQualityHistory(pondId);
-  const history = result.pond_qualities;
+const PondQualityHistory: React.FC<PondQualityHistoryProps> = ({ pondId }) => {
+  const [history, setHistory] = useState<any[]>([]);
+  const [cycleId, setCycleId] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchHistory = async () => {
+      const result = await getPondQualityHistory(pondId);
+      setHistory(result.pond_qualities);
+      setCycleId(result.cycle_id);
+    };
+
+    fetchHistory();
+  }, [pondId]);
 
   return (
     <div className="w-full">
+      <PondQualityDashboard pondId={pondId} cycleId={cycleId} />
+
+      <div className="mt-2"></div>
+
       <div className="flex justify-center">
         <div className="w-[80%] flex gap-4">
           <Waves className="w-10 h-10 text-[#2154C5]" />
           <p className="w-full text-start text-3xl font-semibold">Riwayat Kualitas Kolam</p>
         </div>
       </div>
+      
       <div className="mt-6">
         <DataTable columns={columns} data={history} />
       </div>
@@ -28,4 +46,3 @@ const PondQualityHistory: React.FC<PondQualityHistoryProps> = async ({ pondId })
 };
 
 export default PondQualityHistory;
-
