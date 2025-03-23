5b4dad9ae8623130e78f8812d8b1c694e170a059
novrizair <novrizal.airsyahputra@ui.ac.id>
2025-03-23 21:49:40 +0700

    SCRUM-88 [FEAT] Add new exports in index.tsx

--- lib/fish-sampling/index.tsx
+++ lib/fish-sampling/index.tsx
@@ -1,3 +1,6 @@
 export { fetchLatestFishSampling, fetchFishSamplingHistory} from '@/lib/fish-sampling/fetchFishSampling'
 export { renderSamplingData } from '@/lib/fish-sampling/renderSamplingData'
 export { addFishSampling } from '@/lib/fish-sampling/addFishSampling'
+export { addFishDeath } from '@/lib/fish-sampling/addFishDeath'
+export { getLatestFishDeath } from '@/lib/fish-sampling/getLatestFishDeath'
+
