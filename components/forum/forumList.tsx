7c0e3d8a20ccd9d750e7f424c7e1223098ae56eb
vinamyrnauli <vinasllgn@gmail.com>
2025-04-08 03:46:06 +0700

    FIX error

--- components/forum/forumList.tsx
+++ components/forum/forumList.tsx
@@ -22,7 +22,7 @@ const ForumList: React.FC<ForumListProps> = ({ refresh = 0 }) => {
         const mainForums = data.filter((forum) => forum.parent_id === null);
         setForums(mainForums);
       } catch {
-        setError('Gagal memuat forum');
+        setError('Gagal memuat forumm');
       } finally {
         setLoading(false);
       }
