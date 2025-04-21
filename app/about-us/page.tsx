f9678af915d6d9417f086fa2bb0e0e42e20864ab
Mamdit <varokah@gmail.com>
2025-04-21 21:58:55 +0700

    [GREEN] Penyesuaian desain tampilan Tentang Kami

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,21 +1,12 @@
 // app/contact-us/page.tsx
 import AboutUs from '@/components/about-us/aboutus';
-{/*}
+
 export default function AboutUS() {
   return (
-    <div className="container mx-auto py-8 px-4">
-      <div className="max-w-md mx-auto mt-5">
-        <AboutUs />
-      </div>
-    </div>
+      <div className="max-w-md mx-auto mt-5"> 
+        <AboutUs/>
+      </div> 
   );
-}
-*/}
-export default function AboutUS() {
-  return (
-      <div className="max-w-md mx-auto mt-5">
-        <AboutUs />
-      </div>
-  );
+  
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -41,13 +41,31 @@ export default function Home() {
         </p>
         <TaskByDateList />
       </div>
-      
+      {/*
       <div className="flex flex-col items-center w-full">
-        <p className="mt-10 py-10 pb-5 w-[80%] text-2xl font-medium text-neutral-60">
-          <AboutUS/>
-        </p>
-
+         <AboutUS/>
       </div> 
-    </div> 
+      */}
+      {/* Tentang Kami Section */}
+      <div className="w-full flex justify-center mt-28">
+  <details className="w-full md:w-[60%] rounded-md bg-neutral-100 text-center group">
+    <summary className="px-4 py-3 text-lg text-black font-medium cursor-pointer list-none group-open:mb-4">
+      Tentang Kami
+    </summary>
+    <div className="px-4 pb-4 text-neutral-700 leading-relaxed">
+      <p>
+        Dikembangkan oleh <strong>PT Dimensi Kreasi Nusantara</strong> sejak tahun 2024,
+        aplikasi <strong>BudidayaPlus</strong> dirancang untuk membantu para peternak lele meningkatkan
+        produktivitas dan profit usaha mereka.
+      </p>
+      <p className="mt-2">
+        Kami berharap aplikasi ini menjadi solusi teknologi yang efektif, efisien, dan berdampak nyata bagi seluruh pengguna.
+      </p>
+      <p className="mt-4">Hormat kami, <br /> <strong>PT Dimensi Kreasi Nusantara</strong></p>
+    </div>
+   </details>
+   </div>
+    </div>
+
   );
 }
