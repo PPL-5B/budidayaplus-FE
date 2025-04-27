fef9db48fddd501cfc92c82782f382db4a28a954
novrizair <novrizal.airsyahputra@ui.ac.id>
2025-04-27 22:51:09 +0700

    SCRUM-163-UbahProfilRevamp [GREEN] Implement update profil modal and ensure code coverage 100%

--- components/profile/UpdateProfileModal.tsx
+++ components/profile/UpdateProfileModal.tsx
@@ -2,29 +2,46 @@
 
 import React, { useState } from 'react'
 import { Dialog, DialogTrigger } from '@/components/ui/dialog'
-import { Button } from '@/components/ui/button'
-import { Modal as DialogContent } from '@/components/ui/modal'
+import { DialogContentNoX } from '@/components/ui/dialog-content-no-x'
 import { UpdateProfileForm } from '@/components/profile'
 import { Profile } from '@/types/profile'
-import { UserRoundPen } from 'lucide-react'
+import IconChevronFilled from '@/components/ui/icon-chevron-filled'
 
 interface UpdateProfileModalProps extends React.HTMLAttributes<HTMLDivElement> {
   profile: Profile
+  children: React.ReactNode
 }
-const UpdateProfileModal: React.FC<UpdateProfileModalProps> = ({ profile, ...props }) => {
+
+const UpdateProfileModal: React.FC<UpdateProfileModalProps> = ({ profile, children, ...props }) => {
   const [isModalOpen, setIsModalOpen] = useState(false)
 
   return (
     <div {...props}>
       <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
         <DialogTrigger asChild>
-          <Button className='bg-blue-500 hover:bg-blue-600 active:bg-blue-600'>
-            Update Profile <UserRoundPen className='w-5 h-5 inline-block ml-2' />
-          </Button>
+          {children}
         </DialogTrigger>
-        <DialogContent title='Update Profile'>
+
+        <DialogContentNoX>
+          {/* Wrapper flex relative */}
+          <div className="relative flex items-center justify-center w-full mb-4">
+            {/* Chevron absolute kiri */}
+            <button
+              onClick={() => setIsModalOpen(false)}
+              className="absolute left-0 text-[#2254C5]"
+            >
+              <IconChevronFilled className="h-6 w-6 rotate-[90deg]" fill="#2254C5" />
+            </button>
+
+            {/* Title center */}
+            <h2 className="text-[#2254C5] text-lg font-bold text-center">
+              Ubah Profil
+            </h2>
+          </div>
+
+          {/* Form */}
           <UpdateProfileForm profile={profile} setIsModalOpen={setIsModalOpen} />
-        </DialogContent>
+        </DialogContentNoX>
       </Dialog>
     </div>
   )
