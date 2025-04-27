18b91d15faa913b2fdf988cb6ff1c3fcff5c9fc0
vinamyrnauli <vinasllgn@gmail.com>
2025-04-28 00:32:48 +0700

    SCRUM-134 [FEAT] Add title, tag, upvotes, and downvotes fields to Forum type

--- types/forum/forum.ts
+++ types/forum/forum.ts
@@ -3,9 +3,13 @@ import User from "@/types/auth/user";
 export type Forum = {
     id: string;
     user: User;
+    title?: string;
     description: string;
+    tag: "ikan" | "kolam" | "siklus" | "budidayaplus";
     timestamp: Date;
     parent_id: string | null;
-    replies: Forum[];
+    replies: Forum[]; 
+    upvotes: number;
+    downvotes: number;
 };
 
