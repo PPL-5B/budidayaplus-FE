607ed4aec0f4ca65ff59a439b1da63528d10ecc4
novrizair <novrizal.airsyahputra@ui.ac.id>
2025-05-12 02:26:48 +0700

    SCRUM-227 [GREEN] Implement View Food Sampling History and ensure coverage 100%

--- components/food-sampling/ViewFoodSamplingHistory.tsx
+++ components/food-sampling/ViewFoodSamplingHistory.tsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import { Button } from '@/components/ui/button'
-import { History } from 'lucide-react'
-import Link from 'next/link'
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import { History } from 'lucide-react';
+import Link from 'next/link';
 
 interface ViewFoodSamplingHistoryProps extends React.HTMLAttributes<HTMLDivElement> {
-  pondId: string
+  pondId: string;
 }
 
 const ViewFoodSamplingHistory: React.FC<ViewFoodSamplingHistoryProps> = ({ pondId, ...props }) => {
   return (
-    <div {...props} data-testid='view-pond-quality-history'>
-      <Button variant={'outline'} size={'sm'} asChild>
-        <Link href={`/pond/${pondId}/food-sampling`}>
-          Lihat Riwayat <History className='ml-2' size={16} />
+    <div {...props} data-testid="view-pond-quality-history">
+      <Button
+        asChild
+        variant="outline"
+        size="sm"
+        className="border-[#2154C5] text-[#2154C5] font-semibold hover:bg-[#F1F5FF] px-4 py-2"
+      >
+        <Link href={`/pond/${pondId}/food-sampling`} className="flex items-center gap-2">
+          <History size={16} className="text-[#2154C5]" />
+          Lihat Riwayat
         </Link>
       </Button>
     </div>
-  )
-}
+  );
+};
 
-export default ViewFoodSamplingHistory
+export default ViewFoodSamplingHistory;
