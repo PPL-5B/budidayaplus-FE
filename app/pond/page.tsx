7239dff24e0e7342587382668dd8c2ef3b29439a
khalishana <khalisha.hana10@gmail.com>
2025-05-12 11:57:48 +0700

    [REFACTOR] solve white gap issue in pond page

--- app/pond/page.tsx
+++ app/pond/page.tsx
@@ -8,28 +8,29 @@ import DaftarArtikel from "@/components/artikel/DaftarArtikel";
 
 const PondListPage = async () => {
   const ponds: Pond[] = await fetchPonds();
-  const user = await getUser();
   const cycle = await getLatestCycle();
 
   return (
-    <div className='min-h-[100dvh] flex flex-col items-center py-10 mb-20 bg-[#EAF0FF]'>
-      <div className='w-[80%]'>
-        <div className='flex flex-col space-y-10'>
-  <div>
-    <p className='text-3xl text-center leading-7 font-semibold text-[#2154C5]'>
-      Daftar Kolam
-    </p>
-  </div>
-  {!cycle && <AddPond />}
-  {ponds.length > 0 ? (
-    <PondList ponds={ponds} />
-  ) : (
-    <p className='text-lg text-start'>Tidak ada kolam</p>
-  )}
-</div>
+    <div className='min-h-screen flex flex-col bg-[#EAF0FF]'>
+      <div className='flex-1 flex flex-col items-center pt-4 pb-20'> 
+        <div className='w-[80%]'>
+          <div className='flex flex-col space-y-10'>
+            <div>
+              <p className='text-3xl text-center leading-7 font-semibold text-[#2154C5]'>
+                Daftar Kolam
+              </p>
+            </div>
+            {!cycle && <AddPond />}
+            {ponds.length > 0 ? (
+              <PondList ponds={ponds} />
+            ) : (
+              <p className='text-lg text-start'>Tidak ada kolam</p>
+            )}
+          </div>
 
-        <div className="w-full mt-20">
-          <DaftarArtikel />
+          <div className="w-full mt-20">
+            <DaftarArtikel />
+          </div>
         </div>
       </div>
     </div>
