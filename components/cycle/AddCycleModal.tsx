fdd687160cc74411ed3fa714fb87dbc9314044f9
novrizair <novrizal.airsyahputra@ui.ac.id>
2025-05-18 16:48:16 +0700

    SCRUM-247 [GREEN] Implement AddCycleModal and ensure coverage 100%

--- components/cycle/AddCycleModal.tsx
+++ components/cycle/AddCycleModal.tsx
@@ -2,11 +2,10 @@
 
 import React, { useState } from 'react'
 import { AddCycleForm } from '@/components/cycle'
-import { Dialog, DialogTrigger } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button'
-import { Modal as DialogContent } from '@/components/ui/modal'
 import { Pond } from '@/types/pond';
 import { RefreshCcw } from 'lucide-react';
+import { EmptyPool } from '../ui/empty-pool';
 
 interface AddCycleModalProps extends React.HTMLAttributes<HTMLDivElement> {
   pondList: Pond[]
@@ -17,22 +16,26 @@ const AddCycleModal: React.FC<AddCycleModalProps> = ({ pondList, ...props }) => 
 
   return (
     <div {...props}>
-      {pondList.length === 0 ?
-        (
-          <p className='text-center text-gray-500'>Tidak ada kolam yang tersedia</p>
-        ) :
-        (
-          <Dialog open={isModalOpen} onOpenChange={setIsModalOpen} modal={false}>
-            <DialogTrigger asChild>
-              <Button className='bg-[#ff8585] hover:bg-[#ff8585] text-white rounded-xl'>
-                Mulai Siklus <RefreshCcw className='ml-2 h-5 w-5' />
-              </Button>
-            </DialogTrigger>
-            <DialogContent title='Mulai Siklus'>
-              <AddCycleForm pondList={pondList} setIsModalOpen={setIsModalOpen} />
-            </DialogContent>
-          </Dialog>
-        )}
+      {pondList.length === 0 ? (
+        <EmptyPool />
+      ) : (
+        <>
+          <Button
+            className='bg-[#ff8585] hover:bg-[#ff8585] text-white rounded-xl'
+            onClick={() => setIsModalOpen(true)}
+          >
+            Mulai Siklus <RefreshCcw className='ml-2 h-5 w-5' />
+          </Button>
+
+          {isModalOpen && (
+            <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/30">
+              <div className="relative">
+                <AddCycleForm pondList={pondList} setIsModalOpen={setIsModalOpen} />
+              </div>
+            </div>
+          )}
+        </>
+      )}
     </div>
   )
 }
