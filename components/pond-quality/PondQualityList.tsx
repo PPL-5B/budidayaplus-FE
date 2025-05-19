1d36eb98828eb1ba359ac0766b39339f6484a317
codaaa19 <tsabitiphone@gmail.com>
2025-05-19 19:30:01 +0700

    SCRUM-245: [GREEN]: Implementing listpondquality

--- components/pond-quality/PondQualityList.tsx
+++ components/pond-quality/PondQualityList.tsx
@@ -1,47 +1,58 @@
 'use client';
 
-import { renderQualityData } from '@/lib/pond-quality';
 import { PondQuality } from '@/types/pond-quality';
-import { formatDate } from 'date-fns';
+import { format } from 'date-fns';
 import { id } from 'date-fns/locale';
+import { EmptyData } from '../ui/empty-data';
 import React from 'react';
-import { Badge } from '../ui/badge'
 
 interface PondQualityProps extends React.HTMLAttributes<HTMLDivElement> {
   pondQuality: PondQuality | undefined;
 }
 
 const PondQualityList: React.FC<PondQualityProps> = ({ pondQuality, ...props }) => {
-  const waterQuality = pondQuality ? renderQualityData(pondQuality) : [];
-
   return (
-    <div {...props} data-testid='pond-quality-list'>
+    <div {...props} data-testid="pond-quality-list">
       {pondQuality ? (
-        <div className='mt-5'>
-          <div className='text-gray-500'>
-            <div className='flex'>
-              <p className='font-semibold'>Laporan terakhir</p>
-              <Badge className='ml-2 bg-[#2154C5]'>
-                {pondQuality.reporter.first_name} {pondQuality.reporter.last_name}
-              </Badge>
-            </div>
-            <p> {formatDate(pondQuality.recorded_at, 'EEEE, dd MMMM yyyy', { locale: id })} </p>
-          </div>
-          <div className='grid grid-cols-2 gap-3 mt-4'>
-            {waterQuality.map((item) => (
-              <div key={item.id} className='flex flex-col'>
-                <div className='flex gap-1 items-center'>
-                  <item.icon size={18} />
-                  <p className='text-sm'>{item.label}</p>
-                </div>
-                <p className='text-xl font-semibold text-neutral-600 ml-1 mt-1'> {item.value} </p>
-              </div>
-            ))}
-          </div>
+        <div className="bg-[#F1F5FF] text-[#3B3B3B] p-4 rounded-md border border-[#4D4C4C] mt-4 space-y-2 text-sm">
+          <p>
+            {format(pondQuality.recorded_at, 'EEEE, d MMMM yyyy', { locale: id })},{" "}
+            oleh {pondQuality.reporter.first_name} {pondQuality.reporter.last_name}
+          </p>
+          <p className="font-semibold">
+            Suhu (°C): <span className="font-normal">{pondQuality.water_temperature}°C</span>
+          </p>
+          <p className="font-semibold">
+            pH level: <span className="font-normal">{pondQuality.ph_level}</span>
+          </p>
+          <p className="font-semibold">
+            Salinitas: <span className="font-normal">{pondQuality.salinity}</span>
+          </p>
+          <p className="font-semibold">
+            Kecerahan (cm): <span className="font-normal">{pondQuality.water_clarity}</span>
+          </p>
+          <p className="font-semibold">
+            Sirkulasi: <span className="font-normal">{pondQuality.water_circulation}</span>
+          </p>
+          <p className="font-semibold">
+            DO (mg/L): <span className="font-normal">{pondQuality.dissolved_oxygen}</span>
+          </p>
+          <p className="font-semibold">
+            ORP (mV): <span className="font-normal">{pondQuality.orp}</span>
+          </p>
+          <p className="font-semibold">
+            NH₃ (mg/L): <span className="font-normal">{pondQuality.ammonia}</span>
+          </p>
+          <p className="font-semibold">
+            NO₃ (mg/L): <span className="font-normal">{pondQuality.nitrate}</span>
+          </p>
+          <p className="font-semibold">
+            PO₄ (mg/L): <span className="font-normal">{pondQuality.phosphate}</span>
+          </p>
         </div>
       ) : (
-        <div className='mt-5'>
-          <p className='text-lg text-neutral-600'>Tidak ada data kualitas air</p>
+        <div className="mt-5">
+          <EmptyData />
         </div>
       )}
     </div>
