31d7638f734080d448225db6fea7b6cea641d88f
codaaa19 <tsabitiphone@gmail.com>
2025-05-20 01:11:05 +0700

    [REFACTOR]: Debugging

--- components/pond-quality/PondQualityList.tsx
+++ components/pond-quality/PondQualityList.tsx
@@ -1,24 +1,54 @@
 'use client';
 
+import React, { useEffect, useState } from 'react';
+import { getLatestPondQuality } from '@/lib/pond-quality';
 import { PondQuality } from '@/types/pond-quality';
 import { format } from 'date-fns';
 import { id } from 'date-fns/locale';
+import { Badge } from '@/components/ui/badge';
 import { EmptyData } from '../ui/empty-data';
-import React from 'react';
 
-interface PondQualityProps extends React.HTMLAttributes<HTMLDivElement> {
-  pondQuality: PondQuality | undefined;
+interface PondQualityListProps extends React.HTMLAttributes<HTMLDivElement> {
+  pondId: string;
+  cycleId: string;
 }
 
-const PondQualityList: React.FC<PondQualityProps> = ({ pondQuality, ...props }) => {
+const PondQualityList: React.FC<PondQualityListProps> = ({ pondId, cycleId, ...props }) => {
+  const [pondQuality, setPondQuality] = useState<PondQuality | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPondQuality = async () => {
+      try {
+        const latest = await getLatestPondQuality(pondId, cycleId);
+        if (latest) setPondQuality(latest);
+      } catch (error) {
+        console.error('❌ Gagal mengambil data kualitas kolam:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPondQuality();
+  }, [pondId, cycleId]);
+
+  if (loading) {
+    return <p className="text-neutral-500">Memuat data kualitas kolam...</p>;
+  }
+
   return (
     <div {...props} data-testid="pond-quality-list">
       {pondQuality ? (
         <div className="bg-[#F1F5FF] text-[#3B3B3B] p-4 rounded-md border border-[#4D4C4C] mt-4 space-y-2 text-sm">
-          <p>
-            {format(pondQuality.recorded_at, 'EEEE, d MMMM yyyy', { locale: id })},{" "}
-            oleh {pondQuality.reporter.first_name} {pondQuality.reporter.last_name}
-          </p>
+          <div className="text-gray-500">
+            <div className="flex">
+              <p className="font-semibold">Laporan Terakhir</p>
+              <Badge className="ml-2 bg-[#2154C5]">
+                {pondQuality.reporter.first_name} {pondQuality.reporter.last_name}
+              </Badge>
+            </div>
+            <p>{format(pondQuality.recorded_at, 'EEEE, dd MMMM yyyy', { locale: id })}</p>
+          </div>
           <p className="font-semibold">
             Suhu (°C): <span className="font-normal">{pondQuality.water_temperature}°C</span>
           </p>
@@ -51,7 +81,7 @@ const PondQualityList: React.FC<PondQualityProps> = ({ pondQuality, ...props }) 
           </p>
         </div>
       ) : (
-        <div className="mt-5">
+        <div className="mt-4">
           <EmptyData />
         </div>
       )}
