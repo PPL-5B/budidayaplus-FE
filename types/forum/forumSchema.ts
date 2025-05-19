3c9c2627f4bfb3a62725e4e6621e643ec47190bf
facesofgoblin <buatkuliahrana@gmail.com>
2025-05-19 22:48:54 +0700

    SCRUM-231 [REFACTOR] Merubah tampilan bahasa Indonesia untuk Peringatan Field Kosong

--- types/forum/forumSchema.ts
+++ types/forum/forumSchema.ts
@@ -4,8 +4,8 @@ export const ForumTagEnum = z.enum(['ikan', 'kolam', 'siklus', 'budidayaplus']);
 export type ForumTag = z.infer<typeof ForumTagEnum>;
 
 export const ForumSchema = z.object({
-  title: z.string().min(1, 'Title is required'),
-  description: z.string().min(1, 'Description is required'),
+  title: z.string().min(1, 'Judul tidak boleh kosong'),
+  description: z.string().min(1, 'Deskripsi tidak boleh kosong'),
   tag: ForumTagEnum,
   
   parent_id: z.string().nullable().optional(),
