b3dfe92e381e68505cf9f4e61e559fec94a62980
facesofgoblin <buatkuliahrana@gmail.com>
2025-05-19 11:26:31 +0700

    SCRUM-250 [GREEN] Modify Cycle Carousel and Make Sure All Components Work Well

--- components/cycle/CycleCarousel.tsx
+++ components/cycle/CycleCarousel.tsx
@@ -17,50 +17,52 @@ interface CycleCarouselProps extends React.HTMLAttributes<HTMLDivElement> {
 
 const CycleCarousel: React.FC<CycleCarouselProps> = ({ cycleList, user }) => {
   return (
-    <Carousel
-      opts={{
-        startIndex: cycleList.past.length + cycleList.stopped.length
-      }}
-      className="max-w-full px-[10%]"
-    >
-      <CarouselContent className={cn(
-        '-ml-1',
-      )}>
-        {cycleList.past.map((cycle) =>
-          renderCycleCard(
-            cycle,
-            'Siklus Lalu',
-            'bg-slate-500',
-            'text-slate-400'
-          )
-        )}
-        {cycleList.stopped.map((cycle) =>
-          renderCycleCard(
-            cycle,
-            'Siklus Dihentikan',
-            'bg-slate-500',
-            'text-slate-400',
-          )
-        )}
-        {cycleList.active.map((cycle) =>
-          renderCycleCard(
-            cycle,
-            'Siklus Aktif',
-            'bg-[#2154C5]',
-            'text-primary-300',
-            user
-          )
-        )}
-        {cycleList.future.map((cycle) =>
-          renderCycleCard(
-            cycle,
-            'Siklus Mendatang',
-            'bg-slate-500',
-            'text-slate-400'
-          )
-        )}
-      </CarouselContent>
-    </Carousel>
+<Carousel
+  opts={{
+    startIndex: 0 // Mulai dari siklus aktif (yang ditaruh di awal)
+  }}
+  className="max-w-full px-[10%]"
+>
+  <CarouselContent className={cn('-ml-1')}>
+    {/* Aktif duluan */}
+    {cycleList.active.map((cycle) =>
+      renderCycleCard(
+        cycle,
+        'Siklus Aktif',
+        'bg-[#2154C5]',
+        'text-primary-300',
+        user
+      )
+    )}
+    
+    {cycleList.past.map((cycle) =>
+      renderCycleCard(
+        cycle,
+        'Siklus Lalu',
+        'bg-slate-500',
+        'text-slate-400'
+      )
+    )}
+    
+    {cycleList.stopped.map((cycle) =>
+      renderCycleCard(
+        cycle,
+        'Siklus Dihentikan',
+        'bg-slate-500',
+        'text-slate-400'
+      )
+    )}
+    
+    {cycleList.future.map((cycle) =>
+      renderCycleCard(
+        cycle,
+        'Siklus Mendatang',
+        'bg-slate-500',
+        'text-slate-400'
+      )
+    )}
+  </CarouselContent>
+</Carousel>
   );
 };
 
