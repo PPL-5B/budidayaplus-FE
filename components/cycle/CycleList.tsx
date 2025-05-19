9ea399716f81a49f1c73e5f905346e777954307e
facesofgoblin <buatkuliahrana@gmail.com>
2025-05-19 11:29:45 +0700

    SCRUM-250 [REFACTOR] Add some explanatory comment

--- components/cycle/CycleList.tsx
+++ components/cycle/CycleList.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
-import { getCycleList } from '@/lib/cycle'
-import { CycleCarousel } from '@/components/cycle'
-import { Profile } from '@/types/profile'
+import { getCycleList } from '@/lib/cycle' // Ambil data daftar siklus dari server/backend
+import { CycleCarousel } from '@/components/cycle' // Komponen untuk menampilkan carousel daftar siklus
+import { Profile } from '@/types/profile' // Tipe data untuk user profile
 
+// Tipe props yang diterima komponen, termasuk user
 interface CycleListProps extends React.HTMLAttributes<HTMLDivElement> {
   user?: Profile
 }
 
+// Komponen CycleList adalah komponen async (Server Component)
 const CycleList: React.FC<CycleListProps> = async ({ user, ...props }) => {
+  // Ambil data semua siklus (active, past, etc)
   const cycleList = await getCycleList()
 
   return (
+    // Render container div dengan semua props tambahan (misal: className, style)
     <div {...props}>
+      {/* Render carousel siklus, passing user dan data siklus */}
       <CycleCarousel user={user} cycleList={cycleList} />
     </div>
   )
